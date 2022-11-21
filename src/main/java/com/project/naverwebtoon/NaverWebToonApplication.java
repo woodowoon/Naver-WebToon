109cba679c90bf45e711dffad16d172dfbe856b2
Dowoon <woodw920@gmail.com>
2022-11-21 15:46:34 +0900

    #1 - 프로젝트 세팅
    디펜던시 : lombok, web, Jpa, H2
    추후 추가할 예정

---
+++ src/main/java/com/project/naverwebtoon/NaverWebToonApplication.java
@@ -0,0 +1,13 @@
+package com.project.naverwebtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverWebToonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverWebToonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/naverwebtoon/NaverWebToonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.naverwebtoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverWebToonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
