4da39d60848a24703514970706515f1c209bbf4d
Dowoon <woodw920@gmail.com>
2022-11-28 13:42:51 +0900

    #4 - content, title 공백 시 예외처리
    content, title 공백 시 @Valid, @NoBlank 를 통해서 예외처리

--- src/main/java/com/project/naverwebtoon/global/exception/ExceptionAdvice.java
+++ src/main/java/com/project/naverwebtoon/global/exception/ExceptionAdvice.java
@@ -3,26 +3,35 @@ package com.project.naverwebtoon.global.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
-
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity handleBaseEx(BaseException exception){
-        log.error("BaseException errorMessage(): {}",exception.getExceptionType().getErrorMessage());
-        log.error("BaseException errorCode(): {}",exception.getExceptionType().getErrorCode());
+    public ResponseEntity handleBaseEx(BaseException exception) {
+        log.error("BaseException errorMessage(): {}", exception.getExceptionType().getErrorMessage());
+        log.error("BaseException errorCode(): {}", exception.getExceptionType().getErrorCode());
 
-        return new ResponseEntity(new ExceptionDto(exception.getExceptionType().getErrorCode(), exception.getExceptionType().getErrorMessage()),exception.getExceptionType().getHttpStatus());
+        return new ResponseEntity(new ExceptionDto(exception.getExceptionType().getErrorCode(), exception.getExceptionType().getErrorMessage()), exception.getExceptionType().getHttpStatus());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity handleValidEx(BindException exception){
+        log.error("@ValidException 발생! {}", exception.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+
+        return new ResponseEntity(new ExceptionDto(2000, exception.getBindingResult().getAllErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @Data
     @AllArgsConstructor
-    static class ExceptionDto {
+    static class ExceptionDto{
         private Integer errorCode;
-        private String message;
+        private String errorMessage;
     }
+
 }
