83c7cd717b8c7879a2b78f0cc480a8fa78680b00
Dowoon <woodw920@gmail.com>
2022-11-28 10:02:19 +0900

    #4 - member 예외처리
    - member 의 권한이 관리자가 아닐 때
    - member 가 존재하지 않을 때
    - member 의 info 가 활성화 되어있지 않을 때
    예외처리

--- src/main/java/com/project/naverwebtoon/domain/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/project/naverwebtoon/domain/notice/service/NoticeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.project.naverwebtoon.domain.notice.service;
 
 import com.project.naverwebtoon.domain.member.Member;
+import com.project.naverwebtoon.domain.member.exception.MemberException;
+import com.project.naverwebtoon.domain.member.exception.MemberExceptionType;
 import com.project.naverwebtoon.domain.member.repository.MemberRepository;
 import com.project.naverwebtoon.domain.notice.Notice;
 import com.project.naverwebtoon.domain.notice.dto.CreateNoticeDto;
@@ -20,8 +22,13 @@ public class NoticeServiceImpl implements NoticeService{
     private final NoticeRepository noticeRepository;
 
     @Override
-    public void createNotice(CreateNoticeDto createNoticeDto, String memberId) throws Exception {
-        Member adminMember = checkAdmin(memberId);
+    public void createNotice(CreateNoticeDto createNoticeDto, String memberId){
+        // 이렇게 오류를 일일이 다 불러오지 않고 처음부터 처리해줄 수 있는 방법은 없을까??
+        // 처음부터 오류에 관한 내용을 싹다 처리해버리는 거지.
+        // 근데, 그러려면 memberId 를 넘겨야되는데 어디서 넘기지??
+        checkMemberIsNull(memberId, MemberExceptionType.NOT_FOUND_MEMBER);
+        checkMemberActive(memberId, MemberExceptionType.NOT_MEMBER_ACTIVE);
+        Member adminMember = checkAdmin(memberId, MemberExceptionType.NOT_ADMIN_MEMBER);
         createNoticeDto.setMember_id(adminMember);
 
         Notice notice = new Notice();
@@ -37,8 +44,29 @@ public class NoticeServiceImpl implements NoticeService{
         noticeRepository.saveNotice(notice);
     }
 
+    // TODO : 예외처리는 JPA Data 배우게 되면 그때 orElseThrow 활용
     @Override
-    public Member checkAdmin(String ID) throws Exception {
-        return memberRepository.findById(ID);
+    public Member checkAdmin(String ID, MemberExceptionType memberExceptionType) {
+        Member memberId = memberRepository.findById(ID);
+        if (! memberId.getGrade().toString().equals("Admin")) {
+            throw new MemberException(memberExceptionType);
+        }
+        return memberId;
+    }
+
+    @Override
+    public void checkMemberIsNull(String ID, MemberExceptionType memberExceptionType) {
+        Member memberId = memberRepository.findById(ID);
+        if(memberId == null) {
+            throw new MemberException(memberExceptionType);
+        }
+    }
+
+    @Override
+    public void checkMemberActive(String ID, MemberExceptionType memberExceptionType) {
+        Member memberID = memberRepository.findById(ID);
+        if(! memberID.getInfo().toString().equals("Active")) {
+            throw new MemberException(memberExceptionType);
+        }
     }
 }
